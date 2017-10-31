f22635c18159cc798fbc06e29a6a0b7e04e171cc
lavriz <1lavrov1@ukr.net>
2017-10-31 19:16:25 +0300

    messenger ok, profile started

--- js/main.js
+++ js/main.js
@@ -1,75 +1,220 @@
 $('document').ready(function(){
-	$('#message').click(function(){
-		$(".header__box").toggle();
-		$(".close-wrap").toggle();
-	});
-	$('.close-wrap').click(function(){
-		$(".header__box").toggle();
-		$(".close-wrap").toggle();
-	});
-	$('#inbox-list').click(function(){
-		$(".inbox__list").toggle();
-	});
-	$('#edit-name').click(function(){
-		$("#modal-name").toggle();
-	});
-	$('#edit-avatar').click(function(){
-		$("#modal-avatar").toggle();
-	});
-	$('#edit-about').click(function(){
-		$("#modal-about").toggle();
-	});
+    $('#message').click(function(){
+        $(".header__box").toggle();
+        $(".close-wrap").toggle();
+    });
+    $('.close-wrap').click(function(){
+        $(".header__box").toggle();
+        $(".close-wrap").toggle();
+    });
+    $('#inbox-list').click(function(){
+        $(".inbox__list").toggle();
+    });
+    $('#edit-name').click(function(){
+        $("#modal-name").toggle();
+    });
+    $('#edit-avatar').click(function(){
+        $("#modal-avatar").toggle();
+    });
+    $('#edit-about').click(function(){
+        $("#modal-about").toggle();
+    });
+    $('#edit-video').click(function(){
+        $("#modal-video").toggle();
+    });
     $('#edit-album').click(function(){
         $("#modal-album").toggle();
     });
-	$('#edit-contacts').click(function(){
-		$("#modal-contacts").toggle();
-	});
-	$('#edit-company').click(function(){
-		$("#modal-company").toggle();
-	});
-	$('#edit-languages').click(function(){
-		$("#modal-languages").toggle();
-	});
-	$('#edit-tags').click(function(){
-		$("#modal-tags").toggle();
-	});	
+    $('#edit-contacts').click(function(){
+        $("#modal-contacts").toggle();
+    });
+   
+    $('#edit-company').click(function(){
+        $("#modal-company").toggle();
+    });
+    $('#edit-languages').click(function(){
+        $("#modal-languages").toggle();
+    });
+    $('#edit-tags').click(function(){
+        $("#modal-tags").toggle();
+    }); 
 
-	$('.modal-edit__close, .modal-edit__close-text').click(function(){
-		$(".modal-edit").css("display","none");
-	});
-	$('.dropdown__click_1').click(function(){
-		var text;
-		text = $(this,".dropdown__click_1").text();
-		$(".dropdown__btn_inner_1").html(text);
-	});
+// $('.ac_results').before('#name');
 
-	$('.dropdown__click_2').click(function(){
-		var text;
-		text = $(this,".dropdown__click_2").text();
-		$(".dropdown__btn_inner_2").html(text);
-	});	
-	$('.owl-carousel').owlCarousel({
-	    nav:true,
-	    navText: ["<img src='img/prev.png'>","<img src='img/next.png'>"],
-	    center:false,
-	    loop:true,
-		smartSpeed: 1000,
-		mouseDrag: false,
-		touchDrag: true,
-		fluidSpeed: true,
-	    responsive: {
-	    	0 : {
-	    	  items:1
-	    	},
-	    	768 : {
-	    	  items:3
-	    	},
-	    	1024 : {
-	    		items:3
-	    	}
-	    }
-	});
+//     $('#name').autocomplete(['Женя', 'Витя', 'Гриша', 'Аня', 'Петя'], {
+//         width: 200,
+//         max: 3
+//     });
+
+
+var myObj = [
+    {
+        "name": 'eugene',
+        "img": 'img/avatars.png',
+        "userId": '0'
+        
+    },
+    {
+        "name": 'eugene2',
+        "img": 'img/avatars.png',
+        "userId": '1'
+    },
+    {
+       "name": 'eugene3',
+        "img": 'img/avatars.png',
+        "userId": '2'
+    }
+]
+
+$('.input__removed').keyup(function () {
+     // console.log(myObj.length)
+   
+       
+        if ($(this).val().length >3) {
+             renderAutocomplete(myObj);
+        }
+        
+    });
+$('.youtube_link').change(function(){
+var youLink = $('.youtube_link').val().split('=');
+var finalLink = 'http://img.youtube.com/vi/'+ youLink[1] +'/0.jpg' ;
+
+if ( (youLink[0].search('youtube') != -1) ) {
+$( ".youtube_container" ).append('<div class="youtube_img_wrapper"><img  title="" alt="" src="' + finalLink + '" class="youtube_img" height="180" width="280"/><div class="youtube__cross">✖</div></div>' );
+}
+$('.youtube_img_wrapper').on('click', function(){
+    $(this).remove();
+    // console.log('zaza');
+});
+// $('.youtube_img').attr('src',finalLink )
+});
+function renderAutocomplete(data) {
+    for(var i=0; i<data.length; i++){
+            // console.log(myObj[i]);
+            $('.form-edit__names').css('display','block');
+                     // $( "<div class='modal-edit__names_line'></div>" ).prependTo( "#modal-messenger .modal-edit__names" );    
+                     $( "<div class='form-edit__name_full' href='#' id='user"+data[i].userId+"'><img src='"+ data[i].img +"'><p class='form-edit__soloname'>"+ data[i].name +"</p></div>" ).prependTo( ".form-edit__names" );
+            // $( "<p>"+ myObj[i].name +"</p>" ).prependTo( "#modal-messenger .modal-edit__names_line" );
+
+        }
+};
+   
+if($('.inbox__newmessage').outerHeight() < $('.inbox_width').outerHeight() ){
+    var inbox_height = $('.inbox_width').outerHeight();
+$('.inbox__newmessage').css('height',inbox_height);
+}
+
+$('#compose').click(function(){
+      $('.inbox-box.inbox__allmessages').css('display','none');
+       $('.inbox-box.inbox__newmessage').css('display','block');
+});
+$('.inbox__message').on('click', function(){
+         $('.inbox-box.inbox__allmessages').css('display','block');
+       $('.inbox-box.inbox__newmessage').css('display','none');
+
+$('.section-form__btn').on("click",function(event){
+event.stopPropagation();
+
+});
+});
+// $('.share_brn').on('click', function(event){
+// event.stopPropagation();
+// $('.forma_wrapper_shares').addClass('active');
+// $('.bg').fadeIn(300);
+// $('.forma_wrapper2_shares').addClass('active');
+// $('body').addClass('modal-open');
+
+
+// $('.cross').on("click", function(){
+//     $('.forma_wrapper2_shares').removeClass('active');
+//   $('.forma_wrapper_shares').removeClass('active');
+    
+//       $('.bg').fadeOut(300);
+//       $('body').removeClass('modal-open');
+//       $(document).off("click");
+// });
+// $(document).on("click", function(){
+//     $('.forma_wrapper2_shares').removeClass('active');
+//   $('.forma_wrapper_shares').removeClass('active');
+    
+//       $('.bg').fadeOut(300);
+//       $('body').removeClass('modal-open');
+//       $(document).off("click");
+// });
+// $('#contact-wrapper_shares').on("click",function(event){
+//     event.stopPropagation();
+//   });
+
+// });
+
+
+
+$(document).on("click", function(){
+   $('.form-edit__names').empty();
+   $('.form-edit__names').css('display','none');
+     
+});
+$('.form-edit__names').on("click",function(event){
+event.stopPropagation();
+$('.form-edit__name_full').on('click', function(){
+
+
+
+// $(this).attr('id');
+$('.form-edit__names').css('display','none');
+ 
+var newInput = $(this).children('p').text();
+// $('.input__removed').remove();
+$('.input__removed').val('');
+$('.input__removed').val(newInput);
+// $( "<input type='text' class='form-edit__field input__removed' id='' value='"+ newInput +"' autocomplete='off'>" ).prependTo( ".main_input" );
+});
+
+
+
+  });
+
+
+
+
+    
+
+
+    $('.modal-edit__close, .modal-edit__close-text').click(function(){
+        $(".modal-edit").css("display","none");
+    });
+    $('.dropdown__click_1').click(function(){
+        var text;
+        text = $(this,".dropdown__click_1").text();
+        $(".dropdown__btn_inner_1").html(text);
+    });
+
+    $('.dropdown__click_2').click(function(){
+        var text;
+        text = $(this,".dropdown__click_2").text();
+        $(".dropdown__btn_inner_2").html(text);
+    }); 
+    $('.owl-carousel').owlCarousel({
+        nav:true,
+        navText: ["<img src='img/prev.png'>","<img src='img/next.png'>"],
+        center:false,
+        loop:true,
+        smartSpeed: 1000,
+        mouseDrag: false,
+        touchDrag: true,
+        fluidSpeed: true,
+        responsive: {
+            0 : {
+              items:1
+            },
+            768 : {
+              items:3
+            },
+            1024 : {
+                items:3
+            }
+        }
+    });
 var swiper = new Swiper('.popup .swiper-container', {
     pagination: '.popup .swiper-pagination',
     effect: 'coverflow',
@@ -98,7 +243,12 @@ $('.popup .close_window, .overlay').click(function (){
     $('.popup, .overlay').css({'opacity':'1', 'visibility':'visible'});
     // e.preventDefault();
   });
-	
+
+
+
+
+
+    
 });
 
 //Cropp avatar 
